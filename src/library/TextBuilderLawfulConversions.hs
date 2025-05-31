39411f4d7d83611e21f86e58876ec709cb76fc9a
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-05-31 13:00:08 +0300

    Extend docs

--- src/library/TextBuilderLawfulConversions.hs
+++ src/library/TextBuilderLawfulConversions.hs
@@ -1,5 +1,23 @@
-module TextBuilderLawfulConversions () where
+-- |
+-- Provides instance for conversion between 'TextBuilder' and:
+--
+-- - 'Data.Text.Text'
+-- - 'Data.Text.Lazy.Text'
+-- - 'Data.Text.Lazy.Builder.Builder'
+-- - 'Data.Text.Encoding.StrictBuilder'
+-- - 'Data.Text.Encoding.StrictTextBuilder'
+--
+-- The typeclasses are reexported here for documentation purposes,
+-- letting you know which instances are available.
+module TextBuilderLawfulConversions
+  ( IsSome,
+    IsMany,
+    Is,
+  )
+where
 
+import LawfulConversions (Is, IsMany, IsSome)
+import TextBuilder (TextBuilder)
 import TextBuilderLawfulConversions.LazyBuilder ()
 import TextBuilderLawfulConversions.LazyText ()
 import TextBuilderLawfulConversions.StrictBuilder ()
